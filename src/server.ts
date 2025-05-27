19523640178efa6311e4700250d86caf7fbe1631
nazmulhossen1 <nazmulhossen.main@gmail.com>
2025-05-27 13:26:53 +0600

    Basic projct setup

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose'
+import config from './app/config'
+import app from './app'
+import express from 'express'
+import cors from 'cors'
+
+// parser
+app.use(express.json())
+app.use(cors())
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string)
+    app.listen(config.port, () => {
+      console.log(`server is running on  :  ${config.port}`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+main()
