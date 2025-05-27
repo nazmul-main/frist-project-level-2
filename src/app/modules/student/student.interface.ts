5b8d0595f0f1258d63c4e8aa214421f745e8188e
nazmulhossen1 <nazmulhossen.main@gmail.com>
2025-05-27 22:30:11 +0600

    Create student interface

---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,40 @@
+import { Schema, model, connect } from 'mongoose';
+export type Gardian = {
+    fatherName: string;
+    fatherOccupation: string;
+    fatherContactNo: string;
+    motherName: string;
+    motherOccupation: string;
+    motherContactNo: string;
+}
+
+export type UserName = {
+    fristname: string;
+    middleName: string;
+    lastName: string;
+}
+
+export type LocalGardian ={ 
+    name: string;
+    occupation: string;
+    contact: string;
+    address: string
+}
+
+export type Student = {
+    id: number;
+    name: UserName;
+    gender: "male" | "female";
+    dateOfBrith: string;
+    email: string;
+    avatatar?: string;
+    contactNo: string;
+    emergencyContactNo: string;
+    bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+    presentAddress: string;
+    permanentAdress: string;
+    gardian: Gardian;
+    localGardian: LocalGardian;
+    profileImage?: string
+    isActive: 'active' | 'inActive'
+}
--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ async function main() {
   try {
     await mongoose.connect(config.database_url as string)
     app.listen(config.port, () => {
-      console.log(`server is running on  :  ${config.port}`)
+      console.log(`App   is running on  :  ${config.port}`)
     })
   } catch (error) {
     console.log(error)
